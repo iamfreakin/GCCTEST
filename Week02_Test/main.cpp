f0e228b889ce543f95355a93eed5fa593607fe0d
iamfreakin <qjaqjaqja1877@gmail.com>
2026-04-20 14:49:17 +0900

    feat: 자료형 변수 선언 및 사

--- Week02_Test/main.cpp
+++ Week02_Test/main.cpp
@@ -2,7 +2,52 @@
 
 using namespace std;
 
-int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+int main()
+{
+    char userName[50];
+    char charactorClass[50];
+
+    // 1. 다양한 자료형의 변수 선언 및 초기값 할당
+    int level = 1;
+    int hp = 100;
+    int mp = 100;
+    float attackSpeed = 5.5f;
+
+    // 스탯 시스템
+    int strength = 50;
+    int dexterity = 50;
+    int vitality = 50;
+    int energy = 50;
+
+    // 하드코어 모드 여부 변수
+    bool isHardcore = true;
+
+    cout << "[ Character Creation ]\n";
+    cout << "Input your name : ";
+    cin >> userName;
+    cout << "Input your Class : ";
+    cin >> charactorClass;
+
+    cout << "\n::::::::: Welcome to the Sanctuary :::::::::\n";
+    cout << "User Name : [" << userName << "]\n";
+
+    // 2. 변수에 저장된 데이터 출력
+    cout << "--------------------Character Status--------------------\n";
+    cout << "Class : " << charactorClass << "\n";
+    cout << "Level : " << level << "\n";
+    cout << "HP : " << hp << "\n";
+    cout << "MP : " << mp << "\n";
+    cout << "Attack Speed : " << attackSpeed << "\n";
+    cout << "Strength : " << strength << "\n";
+    cout << "Dexterity : " << dexterity << "\n";
+    cout << "Vitality : " << vitality << "\n";
+    cout << "Energy : " << energy << "\n";
+    cout << "Hardcore Mode : " << isHardcore << " (1: true, 0: false)\n";
+    cout << "--------------------------------------------------------\n";
+
+    // 3. sizeof 연산자를 이용한 메모리 크기 확인
+    cout << "[Memory Check] int type size: " << sizeof(hp) << " bytes\n";
+    cout << "[Memory Check] bool type size: " << sizeof(isHardcore) << " bytes\n";
+
+    return 0;
 }
