e21242f8cd319a3743bd441859671edbba49f21d
iamfreakin <qjaqjaqja1877@gmail.com>
2026-04-29 11:51:38 +0900

    fix: weak_ptr 적용
    - Mercenary 클래스를 소유할 필드를 weak_ptr<Player> 처럼 교체하여 순환참조 메모리 누수 해결

--- Week03_Test/Mercenary.h
+++ Week03_Test/Mercenary.h
@@ -10,12 +10,12 @@ public:
 
     std::string name;
     int attackDamage;
-    shared_ptr<Player> owner;
+    weak_ptr<Player> owner;
     
     Mercenary(const std::string name, int atk, shared_ptr<Player> owner);
     virtual ~Mercenary();
     
     int Attack() const { return attackDamage; }
-    bool isOwnerAilve() const { return owner != nullptr; } // 주인이 살아있는지 확인
+    bool isOwnerAilve() const { return owner.expired(); } // 주인이 살아있는지 확인
     
 };
